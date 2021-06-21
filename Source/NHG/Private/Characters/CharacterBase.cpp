bd936c771389de4356b625bdd08b4687b35094e4
Naveen SJ <56214998+Naveen-SJ@users.noreply.github.com>
2021-06-21 19:45:13 +0530

    Add base character for player and AI

---
+++ Source/NHG/Private/Characters/CharacterBase.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/CharacterBase.h"
+
+// Sets default values
+ACharacterBase::ACharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Default health
+	MaxHealth = 100;
+
+}
+
+// Called when the game starts or when spawned
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Character health
+	CurrentHealth = MaxHealth;
+	
+	
+}
+
+float ACharacterBase::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	const float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	if(IsDead())
+	{
+		// Character already died, can't take any more damage
+		return 0; 
+	}
+
+	CurrentHealth = CurrentHealth - ActualDamage;
+
+	if(IsDead())
+	{
+		if(OnCharacterDeath.IsBound())
+		{
+			OnCharacterDeath.Broadcast(this);
+		}
+	}
+	
+	return ActualDamage;
+}
+
+bool ACharacterBase::IsDead() const
+{
+	return CurrentHealth <= 0;
+}
+
+void ACharacterBase::SetHealth(const float NewHealth)
+{
+	const float OldHealth = CurrentHealth;
+	
+	CurrentHealth = NewHealth;
+	if(CurrentHealth < 0)
+	{
+		CurrentHealth = 0;
+	}
+
+	if(OnCharacterHealthChanged.IsBound())
+	{
+		OnCharacterHealthChanged.Broadcast(this, NewHealth, OldHealth);
+	}
+	
+}
+ 
+
---
+++ Source/NHG/Public/Characters/CharacterBase.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CharacterBase.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCharacterDeathSignature, class ACharacterBase*, Character);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FHealthChangedSignature, class ACharacterBase*, Character, float, NewHealth, float, OldHealth);
+
+UCLASS()
+class NHG_API ACharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	/** Sets default values for this character's properties */
+	ACharacterBase();
+
+protected:
+	
+	//~ Begin AActor overrides
+	virtual void BeginPlay() override;
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+	//~ End AActor overrides
+
+
+	// ---------------------------------
+	// Character health
+	// ---------------------------------
+	
+public:
+	
+	/** Is the character dead or not. */
+	UFUNCTION(BlueprintPure, Category= Character)
+	bool IsDead() const;
+
+protected:
+	/** Sets the current health of the character. */
+	void SetHealth(const float NewHealth);
+ 
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Attributes)
+	float MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, Category= Attributes)
+	float CurrentHealth;
+
+public:
+
+	/** Character death event */
+	UPROPERTY(BlueprintAssignable, Category= Character)
+	FCharacterDeathSignature OnCharacterDeath;
+
+	/** Character health changed event */
+	UPROPERTY(BlueprintAssignable, Category= Character)
+	FHealthChangedSignature OnCharacterHealthChanged;
+	
+	
+
+};
